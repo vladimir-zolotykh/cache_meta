b1c9df344dba65c8c6f2ad074b95918dafd4cfb2
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-11-10 18:28:19 +0200

    Add Singleton (pass through)

--- run.py
+++ run.py
@@ -3,5 +3,18 @@
 # PYTHON_ARGCOMPLETE_OK
 
 
+class Singleton(type):
+    def __call__(cls, *args, **kwargs):
+        print(f"Singleton {cls = }")
+        return super().__call__(*args, **kwargs)
+
+
+class Spam(metaclass=Singleton):
+    def __init__(self):
+        print("creating Spam instance")
+
+
 if __name__ == "__main__":
-    pass
+    s1 = Spam()
+    s2 = Spam()
+    print(s1 is s2)
